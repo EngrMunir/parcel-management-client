73d245c88eb27dcbb06746f992fed4f8eaf604c0
Munir <smsirajulmonir@gmail.com>
2024-07-28 11:13:12 -0700

    password validation implemented

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
         <div>
-        <div className="mx-auto md:w-1/2">
-            <h2 className="text-3xl mb-6 text-center">Please Register</h2>
+        <div className="mx-auto md:w-1/3">
+            <h2 className="text-3xl mb-6 text-center">Please Login</h2>
             <form>
                 <input type="email" placeholder="Email" className="border w-full mb-4 py-2 px-4"/>
                 <br />
                 <input type="password" placeholder="Password" className="border w-full mb-4 py-2 px-4"/>
+                <input className="btn btn-secondary w-full mb-4" type="submit" value="Login" />
             </form>
+            <p className="text-center mb-5">New here? Please <Link className="text-blue-500" to="/register">Register</Link></p>
         </div>
     </div>
     );
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,18 +1,44 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+    const { register, handleSubmit, formState:{errors} } = useForm()
+    const onSubmit = data =>{
+        console.log(data)
+    }
     return (
         <div>
-            <div className="mx-auto md:w-1/2">
+            <div className="mx-auto md:w-1/3">
                 <h2 className="text-3xl mb-6 text-center">Please Register</h2>
-                <form>
-                    <input type="text" placeholder="Your Name" className="border w-full mb-4 py-2 px-4" />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input type="text" {...register('name',{required:true})} placeholder="Your Name" className="border w-full mb-4 py-2 px-4" />
+                    {
+                        errors.name && <span className="text-red-500">Name is required</span>
+                    }
                     <br />
-                    <input type="text" placeholder="Photo URL" className="border w-full mb-4 py-2 px-4"/>
+                    <input type="text" {...register('photo',{required: true})} placeholder="Photo URL" className="border w-full mb-4 py-2 px-4"/>
+                    {
+                        errors.photo && <span className="text-red-500">Name is required</span>
+                    }
                     <br />
-                    <input type="email" placeholder="Email" className="border w-full mb-4 py-2 px-4"/>
+                    <input type="email" {...register('email', {required: true})} placeholder="Email" className="border w-full mb-4 py-2 px-4"/>
+                    {
+                        errors.email && <span className="text-red-500">Name is required</span>
+                    }
                     <br />
-                    <input type="password" placeholder="Password" className="border w-full mb-4 py-2 px-4"/>
+                    <input type="password" {...register('password', {required: true, minLength:6, pattern:/(?=.*[A-Z])(?=.*[a-z])/ })} placeholder="Password" className="border w-full mb-4 py-2 px-4"/>
+                    {
+                        errors.password?.type ==='required' && <span className="text-red-500">Password must be required</span>
+                    }
+                    {
+                        errors.password?.type ==='minLength' && <span className="text-red-500">Password must be at least six character</span>
+                    }
+                    {
+                        errors.password?.type ==='pattern' && <span className="text-red-500">Password must be at least one upper case, one lower case</span>
+                    }
+                     <input className="btn btn-secondary w-full mb-4" type="submit" value="Register" />
                 </form>
+                <p className="text-center mb-5">Already Registered? Please <Link className="text-blue-500" to="/login">Login</Link></p>
             </div>
         </div>
     );
